9ab4fcc6de829bb26e4510c1c446ac3a39b7f2bf
Clem-Fauchet <cfauchet.pro@gmail.com>
2020-06-24 18:17:08 +0200

    Adding NavBar & links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.scss'
+
+//Components
+import NavBar from './components/Navbar'
+
+//Pages
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Our app</h1>
+			<Router>
+				<NavBar />
+				<div className='container'>
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/login' component={Login} />
+						<Route exact path='/signup' component={SignUp} />
+					</Switch>
+				</div>
+			</Router>
 		</div>
 	)
 }
